3d75a08b2bb85ca82b45b79230ba5520c7d3f07f
Burak Yildiz <145865827+burakkyildiz@users.noreply.github.com>
2025-04-22 15:24:40 +0300

    create project

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from rembg import remove
+from PIL import Image
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+input_path = "/Users/burakyildiz/Downloads/input_img.jpg"
+output_path = "/Users/burakyildiz/Downloads/output_img.png" #format must be ".png"
 
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
